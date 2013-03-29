593fc902fe4c2817584433ad7482464691283a52
naddola <naddola2@gmail.com>
2013-03-29 21:02:20 +0900

    first

---
+++ JolP/constants.h
@@ -0,0 +1,14 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+const int WindowWidth  = 900;
+const int WindowHeight = 600;
+
+
+//defines the size of a team -- do not adjust
+//const int TeamSize = 10;
+const int TeamSize = 7;
+
+
+
+#endif
